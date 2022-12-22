f9b4136563d9bb75e3a56f4a86c3d7460ab195ee
Dylan Van Assche <me@dylanvanassche.be>
2022-12-22 16:33:16 +0100

    src: add initial ssc-sensor-proxy project
    Add the meson project based on on other GLib projects
    such as libqmi and eg25-manager. The project compiles and can
    output the current version of the ssc-sensor-proxy.

---
+++ src/ssc-sensor-proxy.c
@@ -0,0 +1,49 @@
+/*
+ * ssc-sensor-proxy: a drop-in replacement for iio-sensor-proxy for SSC support
+ * Copyright (C) 2022 Dylan Van Assche
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "ssc-sensor-proxy.h"
+#include "proxy-version.h"
+
+int main(int argc, char *argv[])
+{
+	g_autoptr(GOptionContext) opt_context = NULL;
+	g_autoptr(GError) err = NULL;
+	struct SSCSensorProxy proxy;
+	gboolean print_version = FALSE;
+	const GOptionEntry options[] = {
+		{ "version", 'v', 0, G_OPTION_ARG_NONE, &print_version, "Print version information and exit.", NULL },
+		{ NULL, 0, 0, G_OPTION_ARG_NONE, NULL, NULL, NULL }
+	};
+
+	opt_context = g_option_context_new ("- iio-sensor-proxy for Qualcomm Sensor Core");
+	g_option_context_add_main_entries (opt_context, options, NULL);
+	if (!g_option_context_parse (opt_context, &argc, &argv, &err)) {
+		g_warning("Parsing CLI options failed: %s", err->message);
+		return -1;
+	}
+
+	if (print_version) {
+		printf("ssc-sensor-proxy version %d.%d.%d\n", PROXY_MAJOR_VERSION, PROXY_MINOR_VERSION, PROXY_PATCH_VERSION);
+		return 0;
+	}
+
+	proxy.loop = g_main_loop_new(NULL, FALSE);
+	g_main_loop_run(proxy.loop);
+
+	return 0;
+}
