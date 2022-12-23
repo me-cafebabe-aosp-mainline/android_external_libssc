cf977d1e350b3897bde3a92d6fa38005e1dfb29f
Dylan Van Assche <me@dylanvanassche.be>
2022-12-23 10:54:58 +0100

    ssc-sensor-proxy: add enable debug logs arg
    Add an argument to enable debug logs which sets internall G_MESSAGES_DEBUG to 'all'

--- src/ssc-sensor-proxy.c
+++ src/ssc-sensor-proxy.c
@@ -25,8 +25,10 @@ int main(int argc, char *argv[])
 	g_autoptr(GError) err = NULL;
 	struct SSCSensorProxy proxy;
 	gboolean print_version = FALSE;
+	gboolean debug = FALSE;
 	const GOptionEntry options[] = {
 		{ "version", 'v', 0, G_OPTION_ARG_NONE, &print_version, "Print version information and exit.", NULL },
+		{ "debug", 'v', 0, G_OPTION_ARG_NONE, &debug, "Enable debug logs.", NULL },
 		{ NULL, 0, 0, G_OPTION_ARG_NONE, NULL, NULL, NULL }
 	};
 
@@ -37,11 +39,20 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
+	/* Print version and exit */
 	if (print_version) {
 		printf("ssc-sensor-proxy version %d.%d.%d\n", PROXY_MAJOR_VERSION, PROXY_MINOR_VERSION, PROXY_PATCH_VERSION);
 		return 0;
 	}
 
+	g_info("ssc-sensor-proxy %d.%d.%d starting", PROXY_MAJOR_VERSION, PROXY_MINOR_VERSION, PROXY_PATCH_VERSION);
+
+	/* Enable debug logs if requested */
+	if (debug)
+		g_setenv ("G_MESSAGES_DEBUG", "all", TRUE);
+		g_debug("Debug messages enabled");
+
+	/* Start GLib main loop */
 	proxy.loop = g_main_loop_new(NULL, FALSE);
 	g_main_loop_run(proxy.loop);
 
