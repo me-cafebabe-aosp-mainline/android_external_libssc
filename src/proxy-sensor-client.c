d7e9036021a06ebb0cd2bbc6e17e8e4c5cc6c279
Dylan Van Assche <me@dylanvanassche.be>
2022-12-25 07:51:13 +0100

    proxy-sensor-client: add minimal QMI communication
    Add a QMI retrieve sensor list to test communication
    over QMI with messages and indications.

--- src/proxy-sensor-client.c
+++ src/proxy-sensor-client.c
@@ -19,12 +19,151 @@
 #include "proxy-sensor-client.h"
 #include "ssc-sensor-proxy.h"
 
+#define UNKNOWN_VALUE 1
+#define SENSOR_DISCOVERY_LEN 41
+
 typedef struct {
 	QmiDevice *device;
 	QmiClientSsc *client;
 	QrtrBus *bus;
 	guint32 node_id;
+	guint indication_report_small_id; // TODO: context_free disconnect
+	guint indication_report_large_id;
 } Context;
+
+guint8 protobuf_sensor_discovery[SENSOR_DISCOVERY_LEN] = {
+	0x0a, 0x12, 0x09, 0xab, 0xab, 0xab, 0xab, 0xab, 0xab, 0xab,
+       	0xab, 0x11, 0xab, 0xab, 0xab, 0xab, 0xab, 0xab, 0xab, 0xab,
+       	0x15, 0x00, 0x02, 0x00, 0x00, 0x1a, 0x04, 0x08, 0x01, 0x10,
+       	0x00, 0x22, 0x08, 0x12, 0x06, 0x0a, 0x00, 0x10, 0x01, 0x18,
+       	0x00
+};
+
+static void
+handle_report(guint64 client_id, GArray *protobuf_data)
+{
+	g_debug("report: client ID: %" G_GUINT64_FORMAT "", client_id);
+	g_debug("report: ProtoBuf data:");
+	for (guint i = 0; i < protobuf_data->len; i++) {
+		guint8 value = g_array_index (protobuf_data, guint8, i);
+		g_print ("\\x%02x", value);
+	}
+	g_printf("\n");
+}
+
+static void
+report_large_received (QmiClientSsc *client,
+	QmiIndicationSscReportLargeOutput *output,
+	gpointer user_data)
+{
+	guint64 client_id;
+	GArray *protobuf_data = NULL;
+	g_autoptr (GError) error = NULL;
+
+	if (!qmi_indication_ssc_report_large_output_get_client_id (output, &client_id, &error)) {
+		g_printerr ("error: cannot extract client ID (large): %s\n", error->message);
+		return;
+	}
+
+	if (!qmi_indication_ssc_report_large_output_get_protobuf_data (output, &protobuf_data, &error)) {
+		g_printerr ("error: cannot extract ProtoBuf data (large): %s\n", error->message);
+		return;
+	}
+
+	handle_report(client_id, protobuf_data);
+}
+
+static void
+report_small_received (QmiClientSsc *client,
+	QmiIndicationSscReportSmallOutput *output,
+	gpointer user_data)
+{
+	guint64 client_id;
+	GArray *protobuf_data = NULL;
+	g_autoptr (GError) error = NULL;
+
+	if (!qmi_indication_ssc_report_small_output_get_client_id (output, &client_id, &error)) {
+		g_printerr ("error: cannot extract client ID (small): %s\n", error->message);
+		return;
+	}
+
+	if (!qmi_indication_ssc_report_small_output_get_protobuf_data (output, &protobuf_data, &error)) {
+		g_printerr ("error: cannot extract ProtoBuf data (small): %s\n", error->message);
+		return;
+	}
+
+	handle_report(client_id, protobuf_data);
+}
+
+static void
+get_sensor_list_ready (QmiClientSsc *client,
+	GAsyncResult *res,
+	gpointer user_data)
+{
+	Context *ctx = user_data;
+	QmiMessageSscControlOutput *output = NULL;
+	g_autoptr (GError) error = NULL;
+
+	output = qmi_client_ssc_control_finish (client, res, &error);
+	if (!output) {
+		g_printerr ("error: operation failed: %s\n", error->message);
+		qmi_message_ssc_control_output_unref (output);
+		return;
+	}
+
+	if (!qmi_message_ssc_control_output_get_result (output, &error)) {
+		g_printerr ("error: couldn't get sensor list: %s\n", error->message);
+		qmi_message_ssc_control_output_unref (output);
+		return;
+	}
+
+	g_debug("Sensor list retrieval success");
+	qmi_message_ssc_control_output_unref (output);
+
+	ctx->indication_report_small_id = g_signal_connect (ctx->client,
+		"report-small",
+		G_CALLBACK (report_small_received),
+		ctx);
+	ctx->indication_report_large_id = g_signal_connect (ctx->client,
+		"report-large",
+		G_CALLBACK (report_large_received),
+		ctx);
+	g_debug("Signals registered for indications");
+}
+
+static void
+get_sensor_list (Context *ctx) {
+	QmiMessageSscControlInput *input = NULL;
+	g_autoptr(GError) error = NULL;
+	g_autoptr (GArray) protobuf = NULL;
+
+	input = qmi_message_ssc_control_input_new ();
+	if (!qmi_message_ssc_control_input_set_unknown_value (input, UNKNOWN_VALUE, &error)) {
+		g_printerr ("error: inserting Unknown Value failed: %s\n", error->message);
+		qmi_message_ssc_control_input_unref (input);
+		return;
+	}
+
+	/* Build protobuf */
+	protobuf = g_array_new(FALSE, FALSE, sizeof (guint8));
+	for (guint i=0; i < SENSOR_DISCOVERY_LEN; i++) {
+		g_array_append_val (protobuf, protobuf_sensor_discovery[i]);
+	}
+
+	if (!qmi_message_ssc_control_input_set_protobuf_data (input, protobuf, &error)) {
+		g_printerr ("error: inserting ProtoBuf data failed: %s\n", error->message);
+		qmi_message_ssc_control_input_unref (input);
+		return;
+	}
+
+	qmi_client_ssc_control (ctx->client,
+		input,
+		10,
+		NULL,
+		(GAsyncReadyCallback)get_sensor_list_ready,
+		ctx);
+	qmi_message_ssc_control_input_unref (input);
+}
 
 static void
 allocate_client_ready (QmiDevice *device, GAsyncResult *result, gpointer user_data) {
@@ -34,10 +173,13 @@ allocate_client_ready (QmiDevice *device, GAsyncResult *result, gpointer user_da
 	ctx->client = QMI_CLIENT_SSC (qmi_device_allocate_client_finish (device, result, &error));
 
 	if (!ctx->client) {
-		g_printerr ("error: couldn't create client for the SSC service: %s\n",
+		g_printerr ("error: couldn't create client for SSC service: %s\n",
 			error->message);
 		exit(EXIT_FAILURE);
 	}
+
+	/* QMI client allocated */
+	get_sensor_list(ctx);
 }
 
 static void
@@ -53,12 +195,11 @@ device_open_ready (QmiDevice *device,
 		exit (EXIT_FAILURE);
 	}
 
-	g_debug ("QMI device at '%s' ready",
-	qmi_device_get_path_display (device));
+	g_debug ("QMI device at '%s' ready", qmi_device_get_path_display (device));
 
-	/* As soon as we get the QmiDevice, create a client for the requested service */
+	/* QMI device opened, allocate client */
 	qmi_device_allocate_client (device,
-		400,
+		QMI_SERVICE_SSC,
 		QMI_CID_NONE,
 		10,
 		NULL,
@@ -81,12 +222,11 @@ device_new_ready (GObject *unused,
 		exit (EXIT_FAILURE);
 	}
 
-	/* Indications are expected as they report sensor data values */
-	//open_flags |= QMI_DEVICE_OPEN_FLAGS_VERSION_INFO;
-	//open_flags |= QMI_DEVICE_OPEN_FLAGS_PROXY;
+	/* Indications are expected as they report all sensor data values */
 	open_flags |= QMI_DEVICE_OPEN_FLAGS_AUTO;
+	open_flags |= QMI_DEVICE_OPEN_FLAGS_EXPECT_INDICATIONS;
 
-	/* Open the device */
+	/* QMI device created, open device */
 	qmi_device_open (ctx->device,
 		open_flags,
 		15,
@@ -116,6 +256,7 @@ bus_new_ready (GObject *source,
 		exit (EXIT_FAILURE);
 	}
 
+	/* QRTR node ready, create QMI device */
 	qmi_device_new_from_node (node,
 		NULL,
 		(GAsyncReadyCallback)device_new_ready,
@@ -124,7 +265,6 @@ bus_new_ready (GObject *source,
 
 gboolean
 sensor_client_init(GFile *file) {
-	/* Open QMI device */
 #if QMI_QRTR_SUPPORTED
 	{
 		g_autofree gchar *id = NULL;
@@ -133,6 +273,7 @@ sensor_client_init(GFile *file) {
 		/* Initialize context */
 		ctx = g_slice_new (Context);
 
+		/* Open node on QRTR bus */
 		id = g_file_get_uri (file);
 		if (qrtr_get_node_for_uri (id, &ctx->node_id)) {
 			g_debug("Opening node ID %" G_GUINT32_FORMAT " on QRTR bus", ctx->node_id);
