5ad0bbb3dd27d73ea9d582d5c531e588448b60ee
Dylan Van Assche <me@dylanvanassche.be>
2024-07-11 16:59:04 +0200

    libssc: add libssc.h header
    Only this header must be included by library users.

---
+++ src/libssc.h
@@ -0,0 +1,28 @@
+/*
+ * libssc: Library to expose Qualcomm Sensor Core sensors
+ * Copyright (C) 2022-2024 Dylan Van Assche
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef _LIBSSC_H_
+#define _LIBSSC_H_
+
+#include <libssc-sensor.h>
+#include <libssc-sensor-accelerometer.h>
+#include <libssc-sensor-compass.h>
+#include <libssc-sensor-light.h>
+#include <libssc-sensor-proximity.h>
+
+#endif /* _LIBSSC_H_ */
