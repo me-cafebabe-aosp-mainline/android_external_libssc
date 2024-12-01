395aafb89644790751bf0f06b631c2d5a9716f08
Dylan Van Assche <me@dylanvanassche.be>
2024-12-01 20:26:59 +0100

    libssc-sensor-compass: listen for measurements after opening
    We disconnect the listening signal on close, so connect it always on open

--- src/libssc-sensor-compass.c
+++ src/libssc-sensor-compass.c
@@ -267,14 +267,27 @@ ssc_sensor_compass_close_sync (SSCSensorCompass *self, GCancellable *cancellable
 static void
 compass_open_ready (SSCSensor *sensor, GAsyncResult *result, gpointer user_data)
 {
+	SSCSensorCompassPrivate *priv = NULL;
+	SSCClient *client = NULL;
 	GTask *task = G_TASK (user_data);
 	g_autoptr (GError) error = NULL;
+
+	priv = ssc_sensor_compass_get_instance_private (SSC_SENSOR_COMPASS (sensor));
 
 	if (!SSC_SENSOR_CLASS (ssc_sensor_compass_parent_class)->open_finish (sensor, result, &error)) {
 		g_task_return_boolean (task, FALSE);
 		g_object_unref (task);
 		return;
 	}
+
+	/* Start listening for reports */
+	g_object_get (SSC_SENSOR (sensor),
+		      SSC_SENSOR_CLIENT, &client,
+		      NULL);
+	priv->report_id = g_signal_connect (client,
+			"report",
+			G_CALLBACK (report_received),
+			sensor);
 
 	g_task_return_boolean (task, TRUE);
 	g_object_unref (task);
@@ -351,8 +364,6 @@ ssc_sensor_compass_init (SSCSensorCompass *self)
 SSCSensorCompass *
 ssc_sensor_compass_new_finish (GAsyncResult *result, GError **error)
 {
-	SSCSensorCompassPrivate *priv = NULL;
-	SSCClient *client = NULL;
 	GObject *sensor;
 	GObject *source;
 
@@ -363,17 +374,6 @@ ssc_sensor_compass_new_finish (GAsyncResult *result, GError **error)
 		g_object_unref (source);
 		return NULL;
 	}
-
-	priv = ssc_sensor_compass_get_instance_private (SSC_SENSOR_COMPASS (sensor));
-
-	/* Start listening for reports */
-	g_object_get (SSC_SENSOR (sensor),
-		      SSC_SENSOR_CLIENT, &client,
-		      NULL);
-	priv->report_id = g_signal_connect (client,
-			"report",
-			G_CALLBACK (report_received),
-			sensor);
 
 	g_object_unref (source);
 	return SSC_SENSOR_COMPASS (sensor);
