839761668faf74b4de624316aaa8c82531cff7f7
Dylan Van Assche <me@dylanvanassche.be>
2024-12-05 20:34:29 +0100

    libssc-sensor-accelerometer: wait until report receiving thread is running during open_sync
    Quickly opening/closing sensor causes deadlocks without synchronization.

--- src/libssc-sensor-accelerometer.c
+++ src/libssc-sensor-accelerometer.c
@@ -27,6 +27,7 @@ enum {
 	N_SIGNALS
 };
 static guint signals[N_SIGNALS];
+gboolean accelerometer_thread_running;
 
 typedef struct _SSCSensorAccelerometerPrivate {
 	guint report_id;
@@ -140,6 +141,9 @@ report_received (SSCClient *self, guint32 msg_id, guint64 uid_high, guint64 uid_
 		}
 
 		ssc_accelerometer_response__free_unpacked (msg, NULL);
+
+		/* Declare that the report receiving thread is running */
+		accelerometer_thread_running = TRUE;
 	}
 }
 
@@ -296,6 +300,7 @@ ssc_sensor_accelerometer_open_sync (SSCSensorAccelerometer *self, GCancellable *
 	success = ssc_sensor_accelerometer_open_finish (self, ctx.result, error);
 
 	/* Start report thread to watch for incoming measurements over QMI indications */
+	accelerometer_thread_running = FALSE;
 	priv->thread = g_thread_new ("report-receiver-accelerometer", report_receiver_thread, self);
 
 	g_main_context_pop_thread_default (priv->context);
@@ -303,7 +308,7 @@ ssc_sensor_accelerometer_open_sync (SSCSensorAccelerometer *self, GCancellable *
 	g_object_unref (ctx.result);
 
 	/* Wait until report receiving thread is running */
-	while (!priv->loop)
+	while (!accelerometer_thread_running)
 		g_thread_yield ();
 
 	return success;
